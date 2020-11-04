5eb362327e25713c967fc12ba494339940247b4a
Ben <benleong0@gmail.com>
2020-11-04 15:01:03 +0000

    adding

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("hi");
+var number = 5; // inline comment
+
+/*
+multiline comment!
+*/
+
+number = 9
+
+// let d = new Date();
+// document.body.innerHTML += "<h1>Today's date is " + d + "</h1>"
+
+function myIncrement() {
+    n = parseInt(document.getElementById("number").innerHTML) + 1
+    document.getElementById("number").innerHTML = n
+  }
