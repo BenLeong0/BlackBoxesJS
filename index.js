8bfb4e39054a215eae70149c607c495a0c8df36c
Ben <benleong0@gmail.com>
2020-11-04 20:28:18 +0000

    Actual mobile support maybe

--- index.js
+++ index.js
@@ -2,10 +2,40 @@ console.log(window.innerHeight)
 console.log(window.innerWidth)
 
 if (window.innerHeight > window.innerWidth) {
-  document.getElementById("mobile").style.display = 'inline'
   console.log('Portrait')
+  document.getElementById("mobile").style.display = 'inline'
+  document.getElementsByTagName("h1")[0].style.fontSize = "600%"
+  document.getElementsByClassName("blackbox")[0].style.maxWidth = "100%"
+  document.getElementsByClassName("blackbox")[1].style.maxWidth = "100%"
+  document.getElementById("number1").style.fontSize = "250%"
+  document.getElementById("number2").style.fontSize = "250%"
+  document.getElementById("askdigits").style.fontSize = "250%"
+
+  digitbuttons = document.getElementsByClassName("digitnumber")
+  var i;
+  for (i = 0; i < digitbuttons.length; i++) {
+    digitbuttons[i].style.fontSize = "200%"
+    digitbuttons[i].style.margin = "0 5"
+  }
+
+  document.getElementById("question_opener").style.width = "100%"
+  document.getElementById("question_opener").style.fontSize = "250%"
+  document.getElementById("questions").style.width = "100%"
+  document.getElementById("questions").style.fontSize = "200%"
+  document.getElementsByTagName("a")[1].style.width = "100%"
+  document.getElementsByTagName("a")[1].style.fontSize = "250%"
+
+  // questions = document.getElementsByClassName("question")
+  // var j;
+  // for (j = 0; j < questions.length; j++) {
+  //   console.log(questions[j])
+  //   questions[j].style.fontSize = "200%"
+  //   questions[j].style.width = "100%"
+  // }
 } else {
   console.log('Landscape')
+  document.getElementById("questions").style.width = "70%"
+  document.getElementById("questions").style.maxWidth = "800px"
 }
 
 var values = []
